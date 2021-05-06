ec89e2ec236fc28308bdbbb4d69b4425ab803fe4
Yatmanov Evgeniy <64483257+moizhirok@users.noreply.github.com>
2021-05-07 00:00:22 +0300

    added themeProvider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'react-jss';
+
+const theme: ITheme = {
+	border: '1px solid #E1E1E1',
+	backgroundColor: '#FBFBFB',
+	filterBorderColor__active: '#4583CC',
+	borderColor: '#E1E1E1',
+	secondaryBackground: '#E5E5E5',
+};
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={theme}>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
---
+++ client/src/interfaces.ts
@@ -0,0 +1,7 @@
+interface ITheme {
+	border: string;
+	backgroundColor: string;
+	filterBorderColor__active: string;
+	borderColor: string;
+	secondaryBackground: string;
+}
