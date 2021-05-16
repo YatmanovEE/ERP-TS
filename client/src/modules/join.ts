daae579267d81cd09ec4269cdb7b06d70ac6d256
Yatmanov Evgeniy <64483257+moizhirok@users.noreply.github.com>
2021-05-16 22:40:24 +0300

    Added the ability to use global classes

--- client/src/modules/join.ts
+++ client/src/modules/join.ts
@@ -1,4 +1,6 @@
-export function joinWithoutDouble(...str: string[]) {
+let globalName: ICreateClassName;
+
+export function joinWithoutDouble(str: string[]): string {
 	return str
 		.filter((strItem, index) => {
 			return str.indexOf(strItem) === index;
@@ -14,10 +16,23 @@ export function createClassName(className: ICreateClassName) {
 	return (...str: string[]) => {
 		let list: string[] = [];
 		str.forEach((item) => {
+			if (Object.keys(globalName).includes(item)) {
+				list.push(globalName[item]);
+			}
 			if (str.includes(item)) {
 				list.push(className[item]);
 			}
+			//TODO Решить как обрабатывать стили, которые совпадают по названию
+			//NOTE Пока вставленные стили вставляются заранее, чем перечеркивают параметры глобальных стилей
 		});
-		return joinWithoutDouble(...list);
+		return joinWithoutDouble(list);
 	};
+}
+
+export function registryGlobalName(className: ICreateClassName): void {
+	if (globalName) {
+		console.error('Уже зарегистрирован один глобальный стиль');
+	} else {
+		globalName = className;
+	}
 }
