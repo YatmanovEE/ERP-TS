f6d17237f797138e0ed31b4c66443458c935dcf8
Yatmanov Evgeniy <64483257+moizhirok@users.noreply.github.com>
2021-05-29 13:37:05 +0300

    For test new component (ModalTemplate)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,31 @@ import { globalStyle } from './App.global.styled';
 import { registryGlobalName } from './modules/join';
 import { CardObject } from './pages/static.CardObject';
 import CreateModalForm from './componentns/Modal';
-
+import { ModalTemplate } from './componentns/ModalTemplate';
+import Modal from './componentns/Modal';
+import { useDispatch } from 'react-redux';
+import { openModal } from './redux/actions/modal';
 function App() {
 	let className = globalStyle();
 	registryGlobalName(className);
+	let dispatch = useDispatch();
+	dispatch(
+		openModal({
+			id: 'id',
+			component: (
+				<ModalTemplate id={'id'} title="fromApp">
+					FromApp
+				</ModalTemplate>
+			),
+		})
+	);
 	return (
 		<>
-		<CardObject></CardObject>
-		{/* // <CreateModalForm title={'Основная информация'} id={'0'}></CreateModalForm> */}
-		{/* // <EventsList list={[]}></EventsList> */}
-		<CreateModalForm></CreateModalForm>
+			{/* <CardObject></CardObject> */}
+			{/* // <CreateModalForm title={'Основная информация'} id={'0'}></CreateModalForm> */}
+			{/* // <EventsList list={[]}></EventsList> */}
+			{/* <CreateModalForm></CreateModalForm> */}
+			<Modal></Modal>
 		</>
 	);
 }
