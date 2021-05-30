a44745b89e125478fde3f0363a7a6a441c163e2a
Yatmanov Evgeniy <64483257+moizhirok@users.noreply.github.com>
2021-05-30 22:49:57 +0300

    Removed create styled, cause component used only global styled

--- client/src/componentns/MenuDivWrapper.tsx
+++ client/src/componentns/MenuDivWrapper.tsx
@@ -1,43 +1,30 @@
 import { FC, useRef, useState } from 'react';
-import { createUseStyles } from 'react-jss';
 import { connect } from 'react-redux';
-import { ITheme } from '..';
-import { createClassName } from '../modules/join';
 import { ToolTipWrapper } from './ToolTipWrapper';
+import { globalStyle } from './../App.global.styled';
 
 namespace IMenuDivWrapper {
-	/**
-	@param children HTMLDivElement in Button;
-	@param component The menu that opens
-	@param modal Modal component
- */
 	export interface Props {
 		children: React.ReactChild;
 		component: React.ReactChild;
 	}
-	export const Style = createUseStyles((theme: ITheme) => ({
-		btn: {
-			outline: 'none',
-			border: 'none',
-			padding: '10px',
-			backgroundColor: 'transparent',
-			cursor: 'pointer',
-		},
-	}));
 }
 
+/**
+@param children HTMLDivElement in Button;
+@param component The menu that opens
+ */
 const MenuDivWrapper: FC<IMenuDivWrapper.Props> = ({
 	children,
 	component,
 }: IMenuDivWrapper.Props) => {
 	let node = useRef(null);
 	const [toolTipState, setToolTipState] = useState(false);
-	const className = IMenuDivWrapper.Style();
-	let join = createClassName(className);
+	let globalName = globalStyle();
 	return (
 		<>
 			<button
-				className={join('menu', 'btn')}
+				className={globalName.btn}
 				ref={node}
 				onClick={() => setToolTipState(!toolTipState)}
 			>
