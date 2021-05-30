ffb459784ed2c4d447987e16b2f4d3a32bbf2d8e
Yatmanov Evgeniy <64483257+moizhirok@users.noreply.github.com>
2021-05-30 12:29:27 +0300

    Added work from session storage for photo section when added new photos

--- client/src/componentns/CardInfo/ModalGeneralInformation.tsx
+++ client/src/componentns/CardInfo/ModalGeneralInformation.tsx
@@ -1,13 +1,18 @@
+import { useEffect, useState } from 'react';
 import { FC } from 'react';
 import { createUseStyles } from 'react-jss';
 import { useDispatch } from 'react-redux';
 import { ITheme } from '../..';
 import { createClassName } from '../../modules/join';
-import { closeModal } from '../../redux/actions/modal';
 import { ModalTemplate } from '../ModalTemplate';
 import CardInfoSection from './CardInfo.CardInfoSection';
+import { PhotoSection } from './CardInfo.GeneralInformation';
 
-function chooseFileHandler(e: React.ChangeEvent<HTMLInputElement>) {
+function chooseFileHandler(
+	e: React.ChangeEvent<HTMLInputElement>,
+	id: string,
+	event: Event
+) {
 	let fileList = e.target.files;
 	if (fileList) {
 		for (let i = 0; i < fileList.length; i++) {
@@ -16,6 +21,22 @@ function chooseFileHandler(e: React.ChangeEvent<HTMLInputElement>) {
 				let fileReader = new FileReader();
 				fileReader.readAsDataURL(file);
 				fileReader.onload = () => {
+					let result = fileReader.result;
+					let get = sessionStorage.getItem(id);
+					let getJSON;
+					if (get) {
+						getJSON = JSON.parse(get);
+					}
+
+					let json = JSON.stringify({
+						General: {
+							photo: getJSON?.General?.photo
+								? getJSON.General.photo.concat(result)
+								: [result],
+						},
+					});
+					sessionStorage.setItem(id, json);
+					window.dispatchEvent(event);
 					//TODO Fetch fileReader.result
 				};
 			}
@@ -27,16 +48,28 @@ namespace IModalGeneral {
 		inputFileSender: {
 			display: 'none',
 		},
-		fileSender: {
+
+		photoContainer: {
+			justifyContent: 'space-between',
+			alignItems: 'center',
+		},
+		placeHolder: {
+			marginLeft: '10px',
+			maxWidth: '60%',
+			color: theme.placeHolderColor,
+		},
+		btn: {
+			display: 'inline-block',
 			background: '#FFFFFF',
 			marginLeft: '-10px',
-			border: '1px solid rgba(0, 0, 0, 0.15)',
 			borderRadius: '1px',
 			boxShadow: theme.boxShadow,
 			transition: 'box-shadow 0.3s ease',
 			'&:active': {
 				boxShadow: 'none',
 			},
+			border: `1px solid rgba(0, 0, 0, 0.15)`,
+			'&:hover': {},
 		},
 	}));
 }
@@ -45,6 +78,28 @@ export const ModalGeneral: FC<{ id: string }> = ({ id }) => {
 	let className = IModalGeneral.Style();
 	let join = createClassName(className);
 	const dispatch = useDispatch();
+
+	const [photo, setPhoto] = useState<string[]>([]);
+	const [event] = useState<Event>(() => {
+		let event = document.createEvent('Event');
+		event.initEvent('session', true, true);
+		return event;
+	});
+	useEffect(() => {
+		function setItem(): void {
+			let photoSrc = sessionStorage.getItem(id);
+			if (photoSrc) {
+				let photoJSON = JSON.parse(photoSrc);
+				setPhoto(photoJSON.General.photo);
+			}
+		}
+		window.addEventListener('session', setItem);
+		setItem();
+		return () => {
+			window.removeEventListener('session', setItem);
+		};
+	}, [id]);
+
 	return (
 		<>
 			<ModalTemplate
@@ -53,17 +108,27 @@ export const ModalGeneral: FC<{ id: string }> = ({ id }) => {
 				onSaveHandler={() => console.log('hello')}
 			>
 				<>
+					{photo.length > 0 && <PhotoSection photoSrc={photo}></PhotoSection>}
 					<CardInfoSection>
-						<input
-							type={'file'}
-							id="fileSender"
-							accept={'.jpeg, .jpg, .png'}
-							className={className.inputFileSender}
-							onChange={(e) => chooseFileHandler(e)}
-						/>
-						<button className={join('btn', 'fileSender')}>
-							<label htmlFor="fileSender">Загрузить файл</label>
-						</button>
+						<div className={join('flex', className.photoContainer)}>
+							<label
+								role={'button'}
+								htmlFor="fileSender"
+								className={join('btn')}
+							>
+								Загрузить файл
+								<input
+									type={'file'}
+									id="fileSender"
+									accept={'.jpeg, .jpg, .png'}
+									className={className.inputFileSender}
+									onChange={(e) => chooseFileHandler(e, id, event)}
+								/>
+							</label>
+							<div className={className.placeHolder}>
+								Вы можете добавить фотографии и другие полезные изображения
+							</div>
+						</div>
 					</CardInfoSection>
 				</>
 			</ModalTemplate>
