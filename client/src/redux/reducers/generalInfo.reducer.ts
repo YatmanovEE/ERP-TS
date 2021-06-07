2cd45b0a0d89d2e8fe8fcc065ddc25825ce88188
Yatmanov Evgeniy <64483257+moizhirok@users.noreply.github.com>
2021-06-08 01:13:15 +0300

    Updated cause added globalReducer and added Add/Remove Link state

--- client/src/redux/reducers/generalInfo.reducer.ts
+++ client/src/redux/reducers/generalInfo.reducer.ts
@@ -1,8 +1,12 @@
 import { Reducer } from 'react';
-import { IGeneralInformation } from '../../componentns/CardInfo/GeneralInformation/GeneralInformation';
+import {
+	IGeneralInformation,
+	ILinkItem,
+} from '../../componentns/CardInfo/GeneralInformation/GeneralInformation';
 import { IGeneralInfo } from '../actions/generalInfo';
 
 import { GeneralInfoTypeActions } from '../types';
+import { addItem, IAddRemoveItem, removeItem } from './globalReducer';
 
 const initialState: IGeneralInformation.Props = {
 	id: 'none',
@@ -15,20 +19,47 @@ export const GeneralInfoReducer: Reducer<
 	IGeneralInformation.Props,
 	IGeneralInfo<IGeneralInformation.Props>
 > = (state = initialState, action) => {
-	switch (action.type) {
-		case GeneralInfoTypeActions.ADDPhoto:
-			return {
-				...state,
-				photoSrc: state.photoSrc.concat(action.payload.photoSrc),
-			};
-		case GeneralInfoTypeActions.REMOVEPhoto:
-			return {
-				...state,
-				photoSrc: state.photoSrc.filter(
-					(photoSrcItem) => photoSrcItem !== action.payload.photoSrc[0] && -1
-				),
-			};
-		default:
-			return state;
+	if (
+		action.type === GeneralInfoTypeActions.ADD_PHOTO ||
+		action.type === GeneralInfoTypeActions.REMOVE_PHOTO
+	) {
+		const photoSrcItem: IAddRemoveItem<IGeneralInformation.Props, string> = {
+			state: state,
+			key: 'photoSrc',
+			arrayState: state.photoSrc,
+			arrayAction: action.payload.photoSrc,
+		};
+		switch (action.type) {
+			case GeneralInfoTypeActions.ADD_PHOTO:
+				return addItem(photoSrcItem);
+			case GeneralInfoTypeActions.REMOVE_PHOTO:
+				return removeItem(photoSrcItem);
+			default:
+				return state;
+		}
 	}
+	if (
+		action.type === GeneralInfoTypeActions.REMOVE_LINK ||
+		action.type === GeneralInfoTypeActions.ADD_LINK
+	) {
+		const linkSection: IAddRemoveItem<
+			IGeneralInformation.Props,
+			ILinkItem.Props
+		> = {
+			state: state,
+			key: 'linkSection',
+			arrayState: state.linkSection,
+			arrayAction: action.payload.linkSection,
+		};
+
+		switch (action.type) {
+			case GeneralInfoTypeActions.ADD_LINK:
+				return addItem(linkSection);
+			case GeneralInfoTypeActions.REMOVE_LINK:
+				return removeItem(linkSection);
+			default:
+				return state;
+		}
+	}
+	return state;
 };
